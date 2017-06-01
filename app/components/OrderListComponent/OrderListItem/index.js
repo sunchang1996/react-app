8cda6716a5e3ea1ae95ace8c81596d72c98865ae
sunchang1996 <1029841435@qq.com>
2017-06-01 10:28:15 +0800

    评价页布局

---
+++ app/components/OrderListComponent/OrderListItem/index.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import './index.less'
+export default class OrderListItem extends Component{
+
+    render(){
+        let {id,commentState,img,title,count,price} = this.props.data;
+        return (
+            <div className="order-list-item">
+                <img src={img} alt=""/>
+                <div className="list-content">
+                    <h3>商户:{title}</h3>
+                    <span>数量{count}</span>
+                    <span>价格:{price}</span>
+                </div>
+                <div className="order-button">
+                    <button>评价</button>
+                </div>
+            </div>
+        )
+    }
+}
--- app/containers/User/subpage/OrderList.js
+++ app/containers/User/subpage/OrderList.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {getOrderList} from  '../../../fetch/orderList'
+import OrderListComponent from "../../../components/OrderListComponent";
 export default class OrderList  extends Component{
     constructor(){
         super();
@@ -11,14 +12,13 @@ export default class OrderList  extends Component{
         return (
             <div>
                 {
-                    // this.state.data?
+                    this.state.data.length? <OrderListComponent data={this.state.data}/>: <div>加载中...</div>
                 }
             </div>
         )
     }
     componentDidMount(){
         getOrderList(this.props.username).then(res=>res.json()).then(data=>{
-            console.log(data);
             this.setState({
                 data
             })
